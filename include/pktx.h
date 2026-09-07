5d38be56fa900495f0851649c7a208cd5ebff10e
Rishav <kooolrishav@gmail.com>
2026-09-07 19:02:22 +0530

    Bump version to 1.2.0

--- include/pktx.h
+++ include/pktx.h
@@ -2,9 +2,9 @@
 #define PKTX_H
 
 #define PKTX_VERSION_MAJOR 1
-#define PKTX_VERSION_MINOR 1
+#define PKTX_VERSION_MINOR 2
 #define PKTX_VERSION_PATCH 0
-#define PKTX_VERSION_STR   "1.1.0"
+#define PKTX_VERSION_STR   "1.2.0"
 #define PKTX_AUTHOR_STR    "Rishav Ambasta"
 
 #endif // PKTX_H
