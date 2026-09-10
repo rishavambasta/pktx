5183505164aafaaa95e5533dbe879b336cc105b1
Rishav <kooolrishav@gmail.com>
2026-09-10 10:12:37 +0530

    bump the version to trigger release of bugfix

--- include/pktx.h
+++ include/pktx.h
@@ -3,8 +3,8 @@
 
 #define PKTX_VERSION_MAJOR 1
 #define PKTX_VERSION_MINOR 3
-#define PKTX_VERSION_PATCH 1
-#define PKTX_VERSION_STR   "1.3.1"
+#define PKTX_VERSION_PATCH 2
+#define PKTX_VERSION_STR   "1.3.2"
 #define PKTX_AUTHOR_STR    "Rishav Ambasta"
 
 #endif // PKTX_H
